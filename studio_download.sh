c1470e020409edbbef02b1cab6149c46ec6c9999
Lokesh Choudhary <lchoudhary@appcelerator.com>
2016-06-16 14:05:15 -0700

    Added platform specific download option & some misc changes

--- studio_download.sh
+++ studio_download.sh
@@ -3,16 +3,34 @@ bold=$(tput bold)
 normal=$(tput sgr0)
 
 download_studio(){
-  echo -n ${bold}"Enter the Appcelerator Studio release version to download (e.g: 4.0.0): "${normal}
+  echo -n -e '\xe2\x9c\x8f' "Enter the platform for the studio download (mac,windows,linux): "
+  read platform
+  echo
+  
+  case "$platform" in
+    "mac") mac_url
+    ;;
+    "windows") windows_url
+    ;;
+    "linux") linux_url
+    ;;
+    *) echo -e '\xe2\x9d\x8c'" Please enter a valid platform."
+    ;;
+  esac
+}
+
+mac_url(){
+  echo -n -e '\xe2\x9c\x8f' "Enter the Appcelerator Studio release version to download (e.g: 4.0.0): "
   read version
   echo
-  echo ${bold}"Fetching & downloading studio "$version" please wait ......."${normal} 
-  echo 
+  echo -e '\xe2\x9c\x8f' "Fetching & downloading studio "$version" for "$platform" please wait ......."
+
   wget -o log.txt -S --spider http://titanium-studio.s3.amazonaws.com/$version/Appcelerator_Studio.dmg
   work_dir=$(pwd)
   c=$(grep -o 'Remote file does not exist' $work_dir/log.txt)
   if [ "$c" == "Remote file does not exist" ]; then
-    echo ${bold}'Specified version not found. Please enter valid studio release version.'${normal}
+    echo
+    echo -e '\xe2\x9d\x8c' ' Specified version not found. Please enter valid studio release version.'
     rm log.txt
     echo
     echo 'Done'
@@ -20,52 +38,102 @@ download_studio(){
     rm log.txt
     echo
     wget http://titanium-studio.s3.amazonaws.com/$version/Appcelerator_Studio.dmg
-    echo -e '\xE2\x9C\x94' ${bold}"File downloaded to :$work_dir/Appcelerator_Studio.dmg"${normal}
+    echo -e '\xE2\x9C\x94' " File downloaded to :$work_dir/Appcelerator_Studio.dmg"
+    echo
+    echo "Done"
+  fi
+}
+
+windows_url(){
+  echo -n -e '\xe2\x9c\x8f' "Enter the Appcelerator Studio release version to download (e.g: 4.0.0): "
+  read version
+  echo
+  echo -e '\xe2\x9c\x8f' "Fetching & downloading studio "$version" for "$platform" please wait ......."
+
+  wget -o log.txt -S --spider http://titanium-studio.s3.amazonaws.com/$version/Appcelerator_Studio.exe
+  work_dir=$(pwd)
+  c=$(grep -o 'Remote file does not exist' $work_dir/log.txt)
+  if [ "$c" == "Remote file does not exist" ]; then
+    echo
+    echo -e '\xe2\x9d\x8c'' Specified version not found. Please enter valid studio release version.'
+    rm log.txt
+    echo
+    echo 'Done'
+  else
+    rm log.txt
+    echo
+    wget http://titanium-studio.s3.amazonaws.com/$version/Appcelerator_Studio.exe
+    echo -e '\xE2\x9C\x94' " File downloaded to :$work_dir/Appcelerator_Studio.exe"
+    echo
+    echo "Done"
+  fi
+}
+
+linux_url(){
+  echo -n -e '\xe2\x9c\x8f' "Enter the Appcelerator Studio release version to download (e.g: 4.0.0): "
+  read version
+  echo
+  echo -e '\xe2\x9c\x8f' "Fetching & downloading studio "$version" for "$platform" please wait ......."
+
+  wget -o log.txt -S --spider "http://titanium-studio.s3.amazonaws.com/$version/appcelerator.studio.linux.gtk.x86_64.zip" 
+  work_dir=$(pwd)
+  c=$(grep -o 'Remote file does not exist' $work_dir/log.txt)
+  if [ "$c" == "Remote file does not exist" ]; then
+    echo
+    echo -e '\xe2\x9d\x8c'' Specified version not found. Please enter valid studio release version.'
+    rm log.txt
+    echo
+    echo 'Done'
+  else
+    rm log.txt
+    echo
+    wget "http://titanium-studio.s3.amazonaws.com/$version/appcelerator.studio.linux.gtk.x86_64.zip" 
+    echo -e '\xE2\x9C\x94' " File downloaded to :$work_dir/appcelerator.studio.linux.gtk.x86_64.zip"
     echo
     echo "Done"
   fi
 }
 
 download_homebrew(){
-  echo ${bold}'Installing homebrew .....'${normal}
+  echo -e '\xe2\x9c\x8f' 'Installing homebrew .....'
   echo
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   # $b
-  echo -e '\xE2\x9C\x94' ${bold}'Homebrew installed successfully .....'${normal}
+  echo -e '\xE2\x9C\x94' ' Homebrew installed successfully .....'
 }
 
 download_wget(){
-  echo ${bold}'Installing wget .....'${normal}
+  echo -e '\xe2\x9c\x8f' 'Installing wget (one time installation) .....'
   echo
   brew install wget
   echo
-  echo -e '\xE2\x9C\x94' ${bold}'Wget installed successfully .....'${normal}
+  echo -e '\xE2\x9C\x94' ' Wget installed successfully .....'
 }
 
 echo
-echo ${bold}'First checking if wget is installed......'${normal}
+echo -e '\xe2\x9c\x8f' 'First checking if wget is installed......'
 b=$(wget -Version|grep -o 'Wget'|tail -1)
 
 if [ "$b" == "Wget" ]; then
   echo
-  echo -e '\xE2\x9C\x94' ${bold}'Wget already installed .....'${normal}
+  echo -e '\xE2\x9C\x94' ' Wget already installed .....'
   echo
   download_studio
   echo
 else
-  echo ${bold}'Wget not installed .....'${normal}
+  echo -e '\xe2\x9d\x8c' 'Wget not installed .....'
   echo
-  echo ${bold}"Checking if homebrew is installed ...."${normal}
+  echo "Checking if homebrew is installed ...."
   a=$(brew -v|grep 'Homebrew'|cut -c1-8)
   echo $b
   if [ "$a" == "Homebrew" ]; then
     download_wget
     download_studio
   else
-    echo ${bold}'Homebrew not installed. Installing homebrew .....'${normal}
+    echo -e '\xe2\x9d\x8c' 'Homebrew not installed. Installing homebrew (one time installation).....'
     echo
     download_homebrew
-    echo -e '\xE2\x9C\x94' ${bold}'homebrew installed successfully.....'${normal}
+    echo -e '\xE2\x9C\x94' ' Homebrew installed successfully.....'
     echo
     download_wget
     echo 
